126ac490951462b66464d1af6ab98300952ca26d
Guiherme-Aguiar <gui9627oli@gmail.com>
2022-02-09 19:14:00 -0200

    Solução do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,60 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) =>  user.username === username);
+
+  if (!user) return response.status(404).json({"error":"User doesn't exists!"});
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length >= 10) return response.status(403).json({"error":"TODOS limit reached, consider upgrading to pro!"});
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({"error":"Given id is invalid!"});
+  }
+
+  selectedUser = users.find(user => user.username === username);
+
+  if(!selectedUser) return response.status(404).json({"error":"User doesn't exists!"});
+
+  const todo = selectedUser.todos.find(todo => todo.id === id);
+
+  if (!todo) return response.status(404).json({"error":"TODO doesn't exists!"});
+
+  request.user = selectedUser;
+  request.todo = todo;
+
+  next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const selectedUser = users.find(user => user.id === id);
+
+  if(!selectedUser) return response.status(404).json({"error":"User with id " + id + " does not exist!"});
+
+  request.user = selectedUser;
+
+  next();
+
+
 }
 
 app.post('/users', (request, response) => {
